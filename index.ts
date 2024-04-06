92a195dbfbd0a102ca4148614e62e8efa7fdcc01
minemobs <issahakim94@gmail.com>
2024-04-06 15:16:04 +0200

    Added GTK Box, GTK button and GTK Label constuctors, Widget align, Widget spacing

--- index.ts
+++ index.ts
@@ -1,7 +1,8 @@
-import { dlopen, ptr, read, JSCallback } from "bun:ffi";
+import { dlopen, JSCallback, suffix } from "bun:ffi";
 import type { Pointer } from "bun:ffi";
+import { Align, Orientation } from "./utils";
 
-const libGTK = dlopen("libgtk-4.so", {
+const libGTK = dlopen(`libgtk-4.${suffix}`, {
   gtk_get_major_version: {
     returns: "uint32_t"
   },
@@ -32,6 +33,33 @@ const libGTK = dlopen("libgtk-4.so", {
   g_signal_connect_data: {
     args: ["ptr", "cstring", "callback", "ptr", "ptr", "int8_t"],
   },
+  gtk_box_new: {
+    args: ["uint8_t", "uint32_t"],
+    returns: "ptr"
+  },
+  gtk_widget_set_halign: {
+    args: ["ptr", "uint8_t"]
+  },
+  gtk_widget_set_valign: {
+    args: ["ptr", "uint8_t"]
+  },
+  gtk_window_set_child: {
+    args: ["ptr", "ptr"]
+  },
+  gtk_box_append: {
+    args: ["ptr", "ptr"]
+  },
+  gtk_label_new: {
+    args: ["cstring"],
+    returns: "ptr"
+  },
+  gtk_button_new_with_label: {
+    args: ["cstring"],
+    returns: "ptr"
+  },
+  gtk_box_set_spacing: {
+    args: ["ptr", "int8_t"]
+  },
 });
 
 // const toCString = (str: string) => new TextEncoder().encode(str + "\0");
@@ -39,10 +67,24 @@ const toCString = (str: string) => Buffer.from(str + "\0", "utf-8");
 
 const testCallback = new JSCallback(
   (app: Pointer, _) => {
-    const window = libGTK.symbols.gtk_application_window_new(app);
-    libGTK.symbols.gtk_window_set_title(window, toCString("Bun GTK"));
-    libGTK.symbols.gtk_window_set_default_size(window, 800, 450);
-    libGTK.symbols.gtk_window_present(window);
+    const gtk = libGTK.symbols;
+    const window = gtk.gtk_application_window_new(app);
+    gtk.gtk_window_set_title(window, toCString("Bun GTK"));
+    gtk.gtk_window_set_default_size(window, 800, 450);
+
+    const box = gtk.gtk_box_new(Orientation.GTK_ORIENTATION_VERTICAL);
+    gtk.gtk_box_set_spacing(box, 30);
+    gtk.gtk_widget_set_halign(box, Align.GTK_ALIGN_CENTER);
+    gtk.gtk_widget_set_valign(box, Align.GTK_ALIGN_CENTER);
+
+    gtk.gtk_window_set_child(window, box);
+    const label = gtk.gtk_label_new(toCString("Hello World"));
+    gtk.gtk_box_append(box, label);
+
+    const button = gtk.gtk_button_new_with_label(toCString("Goodbye World"));
+    gtk.gtk_box_append(box, button);
+
+    gtk.gtk_window_present(window);
   },
   {
     args: ["ptr", "ptr"]  
@@ -54,4 +96,5 @@ const app = libGTK.symbols.gtk_application_new(toCString("fr.minemobs.bun-gtk"),
 libGTK.symbols.g_signal_connect_data(app, toCString("activate"), testCallback.ptr, null, null, 0);
 const status = libGTK.symbols.g_application_run(app, 0, null);
 libGTK.symbols.g_object_unref(app);
+testCallback.close();
 libGTK.close();
